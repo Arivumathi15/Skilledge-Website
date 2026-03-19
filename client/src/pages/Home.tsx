96d653a8d6c5e81b163b340903da0ec04d935135
arivumathimr <55977636-arivumathimr@users.noreply.replit.com>
2026-03-19 02:54:00 +0000

    Restored to '326e1a8307314ddae05a9feb2f7d5967e7fd18b6'
    Replit-Restored-To: 326e1a8307314ddae05a9feb2f7d5967e7fd18b6

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -22,6 +22,8 @@ import {
 import heroImage from "@/assets/images/online-tutoring.jpg";
 import logoImage from "@/assets/images/logo.png";
 
+import growth from "@assets/growth.jpg";
+
 export default function Home() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [showAllTestimonials, setShowAllTestimonials] = useState(false);
